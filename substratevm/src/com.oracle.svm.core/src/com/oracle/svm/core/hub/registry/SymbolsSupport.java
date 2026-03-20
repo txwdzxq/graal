0713b211d4f281cb8892fdcaa863a46371366fc6
Loic Ottet <loic.ottet@oracle.com>
2026-03-20 14:28:05 +0100

    Copy original SymbolsImpl methods instead of relying on substitution original method access

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/registry/SymbolsSupport.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/registry/SymbolsSupport.java
@@ -24,6 +24,11 @@
  */
 package com.oracle.svm.core.hub.registry;
 
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+
 import org.graalvm.nativeimage.ImageInfo;
 import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
@@ -31,7 +36,6 @@ import org.graalvm.nativeimage.Platforms;
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-import com.oracle.svm.core.annotate.TargetElement;
 import com.oracle.svm.espresso.classfile.descriptors.ByteSequence;
 import com.oracle.svm.espresso.classfile.descriptors.NameSymbols;
 import com.oracle.svm.espresso.classfile.descriptors.SignatureSymbols;
@@ -115,12 +119,21 @@ public final class SymbolsSupport {
  */
 @TargetClass(className = "com.oracle.svm.espresso.classfile.descriptors.SymbolsImpl")
 final class Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl {
+    @Alias//
+    ConcurrentHashMap<ByteSequence, Symbol<?>> strongMap;
+
+    @Alias//
+    WeakHashMap<ByteSequence, WeakReference<Symbol<?>>> weakMap;
+
+    @Alias//
+    ReadWriteLock readWriteLock;
+
     @Substitute
     @SuppressWarnings({"static-method"})
     public <T> Symbol<T> lookup(ByteSequence byteSequence) {
         for (var singleton : SymbolsSupport.layeredSingletons()) {
             var symbols = SubstrateUtil.cast(singleton.symbols, Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl.class);
-            Symbol<T> symbol = symbols.originalLookup(byteSequence);
+            Symbol<T> symbol = Util_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl.originalLookup(symbols, byteSequence);
             if (symbol != null) {
                 return symbol;
             }
@@ -128,28 +141,107 @@ final class Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl {
         return null;
     }
 
-    @Alias
-    @TargetElement(name = "lookup")
-    public native <T> Symbol<T> originalLookup(ByteSequence byteSequence);
-
     @Substitute
-    public <T> Symbol<T> getOrCreate(ByteSequence byteSequence, boolean ensureStrongReference) {
+    <T> Symbol<T> getOrCreate(ByteSequence byteSequence, boolean ensureStrongReference) {
         for (var singleton : SymbolsSupport.layeredSingletons()) {
             var symbols = SubstrateUtil.cast(singleton.symbols, Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl.class);
-            Symbol<T> symbol = symbols.originalLookup(byteSequence);
+            Symbol<T> symbol = Util_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl.originalLookup(symbols, byteSequence);
             if (symbol != null && !(ensureStrongReference && symbols.isWeak(symbol))) {
                 return symbol;
             }
         }
-        return originalGetOrCreate(byteSequence, ensureStrongReference);
+        return Util_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl.originalGetOrCreate(this, byteSequence, ensureStrongReference);
     }
 
     @Alias
-    @TargetElement(name = "getOrCreate")
-    public native <T> Symbol<T> originalGetOrCreate(ByteSequence byteSequence, boolean ensureStrongReference);
-
-    @Alias
     public native boolean isWeak(Symbol<?> symbol);
+}
+
+final class Util_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl {
+    @SuppressWarnings("unchecked")
+    static <T> Symbol<T> originalLookup(Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl symbols, ByteSequence byteSequence) {
+        // Lock-free fast path, common symbols are usually strongly referenced e.g.
+        // Ljava/lang/Object;
+        Symbol<T> result = (Symbol<T>) symbols.strongMap.get(byteSequence);
+        if (result != null) {
+            return result;
+        }
+        symbols.readWriteLock.readLock().lock();
+        try {
+            result = (Symbol<T>) symbols.strongMap.get(byteSequence);
+            if (result != null) {
+                return result;
+            }
+            WeakReference<Symbol<?>> weakValue = symbols.weakMap.get(byteSequence);
+            if (weakValue != null) {
+                return (Symbol<T>) weakValue.get();
+            } else {
+                return null;
+            }
+        } finally {
+            symbols.readWriteLock.readLock().unlock();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> Symbol<T> originalGetOrCreate(Target_com_oracle_svm_espresso_classfile_descriptors_SymbolsImpl symbols, ByteSequence byteSequence, boolean ensureStrongReference) {
+        // Lock-free fast path, common symbols are usually strongly referenced e.g.
+        // Ljava/lang/Object;
+        Symbol<T> symbol = (Symbol<T>) symbols.strongMap.get(byteSequence);
+        if (symbol != null) {
+            return symbol;
+        }
+
+        symbols.readWriteLock.writeLock().lock();
+        try {
+            // Must peek again within the lock because the symbol may have been promoted from weak
+            // to strong by another thread; querying only the weak map wouldn't be correct.
+            symbol = (Symbol<T>) symbols.strongMap.get(byteSequence);
+            if (symbol != null) {
+                return symbol;
+            }
+
+            if (ensureStrongReference) {
+                WeakReference<Symbol<?>> weakValue = symbols.weakMap.remove(byteSequence);
+                if (weakValue != null) {
+                    // Promote weak symbol to strong.
+                    symbol = (Symbol<T>) weakValue.get();
+                    // The weak symbol may have been collected.
+                    if (symbol != null) {
+                        symbols.strongMap.put(symbol, symbol);
+                        return symbol;
+                    }
+                }
+
+                // Create new strong symbol.
+                symbol = Target_com_oracle_svm_espresso_classfile_descriptors_Symbols.createSymbolInstanceUnsafe(byteSequence);
+                symbols.strongMap.put(symbol, symbol);
+                return symbol;
+            } else {
+                WeakReference<Symbol<?>> weakValue = symbols.weakMap.get(byteSequence);
+                if (weakValue != null) {
+                    symbol = (Symbol<T>) weakValue.get();
+                    // The weak symbol may have been collected.
+                    if (symbol != null) {
+                        return symbol;
+                    }
+                }
+
+                // Create new weak symbol.
+                symbol = Target_com_oracle_svm_espresso_classfile_descriptors_Symbols.createSymbolInstanceUnsafe(byteSequence);
+                symbols.weakMap.put(symbol, new WeakReference<>(symbol));
+                return symbol;
+            }
+        } finally {
+            symbols.readWriteLock.writeLock().unlock();
+        }
+    }
+}
+
+@TargetClass(Symbols.class)
+final class Target_com_oracle_svm_espresso_classfile_descriptors_Symbols {
+    @Alias
+    static native <T> Symbol<T> createSymbolInstanceUnsafe(ByteSequence byteSequence);
 }
 
 @TargetClass(SymbolsSupport.class)
