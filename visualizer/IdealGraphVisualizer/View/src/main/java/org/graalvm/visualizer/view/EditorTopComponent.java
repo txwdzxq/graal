fd13c85fbe4cf1e59d00f800781775803b93dc60
Andreas Woess <andreas.woess@oracle.com>
2026-04-03 01:37:56 +0200

    Fix missing tooltips.

--- visualizer/IdealGraphVisualizer/View/src/main/java/org/graalvm/visualizer/view/EditorTopComponent.java
+++ visualizer/IdealGraphVisualizer/View/src/main/java/org/graalvm/visualizer/view/EditorTopComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -565,6 +565,11 @@ public final class EditorTopComponent extends TopComponent implements PropertyCh
             if (item instanceof AbstractButton) {
                 AbstractButton button = (AbstractButton) item;
                 processButton(button);
+                if (button.getToolTipText() == null) {
+                    String desc = (String) a.getValue(Action.SHORT_DESCRIPTION);
+                    String name = (String) a.getValue(Action.NAME);
+                    button.setToolTipText(desc != null ? desc : name);
+                }
             }
             // end 
             String n = (String) a.getValue(Action.NAME);
