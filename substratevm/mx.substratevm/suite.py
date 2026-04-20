7ca3edf845300145ca2e87e7a715e42421c779d1
Paul Wögerer <paul.woegerer@oracle.com>
2026-04-20 10:41:54 +0200

    Enable mx unittest for driver tests

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -2726,6 +2726,21 @@ suite = {
             "testDistribution" : True,
         },
 
+        "SVM_DRIVER_TESTS" : {
+          "subDir": "src",
+          "relpath" : True,
+          "dependencies" : [
+            "com.oracle.svm.driver.test",
+          ],
+          "unittestConfig" : "svm-driver-unittest",
+          "distDependencies": [
+            "mx:JUNIT_TOOL",
+            "sdk:NATIVEIMAGE",
+            "SVM_DRIVER",
+          ],
+          "testDistribution" : True,
+        },
+
         "SVM_TESTS" : {
           "subDir": "src",
           "relpath" : True,
